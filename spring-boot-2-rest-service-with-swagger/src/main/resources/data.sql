cf12b6c617e30ef7a6ad157a47e4bd76ed2483a4
Dewi Jones <dewijones92@gmail.com>
2023-04-12 09:34:04 +0000

    more

--- spring-boot-2-rest-service-with-swagger/src/main/resources/data.sql
+++ spring-boot-2-rest-service-with-swagger/src/main/resources/data.sql
@@ -4,3 +4,10 @@
 
 
 select * from person
+
+
+insert ignore into person (id, name) values (1, "dewi")
+insert ignore into cat (id, colour, location, name) values (1, "black", "swansea", "dewi")
+
+
+insert ignore into person_cats (person_id, cats_id)  values (1,1)
